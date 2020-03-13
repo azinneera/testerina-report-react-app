b630921fab1fad88364fc8a880f6f93762d5f94d
Joseph Fonseka <hevayo@gmail.com>
2020-03-13 10:37:21 +0530

    Add test report component

---
+++ src/Report.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'; // we need this to make JSX compile
+
+type TestData = {
+    
+}
+
+export const Report = ( data: TestData ) => <aside>
+     <p>Test Report</p>
+</aside>
